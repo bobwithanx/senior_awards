5426020ccf93f66a622a806f0d0cc4c7260380c7
Bob Marquis <bob@sailbear.net>
2018-04-13 22:05:48 -0700

    Initial commit

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,24 @@
+class StudentsController < ApplicationController
+  def index
+    @students = Student.all
+  end
+
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+  end
+
+  def create
+    @student = Student.new(student_params)
+
+    @student.save
+    redirect_to @student
+  end
+
+  private
+    def student_params
+      params.require(:student).permit(:firstname, :lastname, :id_number, :blurb)
+    end
+end
