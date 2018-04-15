3c783b0886a39506108c273451f27d4acd1843ad
Bob Marquis <bob@sailbear.net>
2018-04-14 23:53:25 -0700

    Designed popup layout

--- Gemfile
+++ Gemfile
@@ -5,6 +5,9 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+gem "bulma-rails", "~> 0.6.2"
+gem "font-awesome-rails"
+gem 'rails_admin', '~> 1.3'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.1.5'
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   get 'welcome/index'
 
-  resources :students
-
+  resources :students do
+    get 'popup', on: :member
+  end
+  
   root 'welcome#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
