4a541189f955069cb56d3f6661887cd8259431a0
Bob Marquis <bob@sailbear.net>
2018-05-07 00:19:05 -0700

    Visual adjustments.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,7 @@
 module ApplicationHelper
   def display_image(url)
     url = Rails.application.assets.find_asset(url).nil? ? nil : url
-    image_tag url || 'photos/default_image.jpg'
+    # image_tag url || 'photos/default_image.jpg'
+    image_tag url || ''
   end
 end
