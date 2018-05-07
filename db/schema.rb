f93047d67fad8418f6063fdece1e81d72620772a
Bob Marquis <bob@sailbear.net>
2018-05-07 00:03:06 -0700

    Added photo field to Student model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180506144457) do
+ActiveRecord::Schema.define(version: 20180507070139) do
 
   create_table "advisories", force: :cascade do |t|
     t.string "name"
@@ -87,6 +87,7 @@ ActiveRecord::Schema.define(version: 20180506144457) do
     t.datetime "updated_at", null: false
     t.integer "advisory_id"
     t.integer "counselor_id"
+    t.string "photo"
     t.index ["advisory_id"], name: "index_students_on_advisory_id"
     t.index ["counselor_id"], name: "index_students_on_counselor_id"
   end
